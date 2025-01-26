1e75cb3297d19ffd37bcb1a76ee717dc66a0d052
Gravity4543 <vatsshikhar130@outlook.com>
2025-01-26 13:46:09 +0530

    cloning sort repo

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# Sorting_algo_CI-CD
-project where it shows the different algorithm sorted 
+# algorithm-visualizer
+
+An application for visualizing sorting algorithms.
+
+This is my freshman year summer project!  Trying to get a better understanding of sorting algorithms before my Data Structures and Algorithms class this fall while applying new things i've learned such as Swing, Git, Maven, and multi-threading.
+
+Despite this type of project being unoriginal, the code itself is original.
+
+<!-- ![Startup](https://github.com/dlarocque/AlgorithmVisualizer/blob/master/images/Startup.PNG) -->
+
+[![AlgorithmVisualizer](https://github.com/dlarocque/AlgorithmVisualizer/blob/master/images/Startup.PNG)](https://www.youtube.com/watch?v=WDOpFcnzuaQ "AlgorithmVisualizer")
+
+## Installation
+
+### Prerequisite
+
+*You must have the JDK installed on your system [Download JDK](https://www.oracle.com/java/technologies/javase-downloads.html)*
+
+*Have Apache Maven version 3.6.3 or later installed on your computer* (If you want to modify source code)
+
+-----------------------------------------------
+
+#### Clone
+
+`git clone https://github.com/dlarocque/AlgorithmVisualizer`
+
+#### Download ZIP
+
+Download the .zip file from https://github.com/dlarocque/AlgorithmVisualizer and extract its contents
+
+## Running
+
+*For Linux/macOS users*
+
+From the root of the project, build and execute simultaneously:
+
+`mvn install exec:java -Dexec.mainClass=com.example.algorithmvisualizer.Main`
+
+## How to use 
+
+On startup, the application will open up a frame containing buttons and an unsorted array in bar form.  There is a Reset button, a button for each sorting algorithm, a drop down size changer, an FPS slider, and a label showing statistics.
+
+_The Reset Button_ will re-shuffle the array and stop any sorting that is being done, while keeping the size set to the array.
+
+_The Sort Buttons_ will be available when sorting isn't being done.  Clicking any of them will start sorting the displayed array with the selected sorting algorithm.
+
+_The Size Changer_ is initially set at N = 10, clicking it will open a drop down menu listing the available array sizes, clicking any of them will stop sorting and display a new array of the chosen size.
+
+_The FPS Slider_ is initialially set at 10 fps, it ranges from 1 fps all the way to 100 fps.  This slider can be used to speed up the sorting / visualization process on bigger arrays or slow it down to get a better understanding of the sorting process on smaller arrays.  It can be used before, during, and after sorting.
+
+_The Performance Label_ between the array and the buttons displays all of the stats that can help the user understand how different sorting algorithms perform compared to each other.  (It's interesting to see how bubble sort is better performing than merge and quick sort on small arrays)
+
+#### Notes
+
+The time to visualize the sorting of one of the algorithms is not a good representation of its real sorting time.  This is because the arr is updated every time there is an index swap, this makes it so that sorting algorithms with fewer swaps are visualized much faster.  This is the reasoning behind including a 'Sorting Time' stat along with 'Visualzation Time' in the Performance label.
+
+## Authors
+
+- [dlarocque](https://github.com/dlarocque)
+
+## License
+
+This project is licensed under the MIT License.  See [LICENSE.txt](https://github.com/dlarocque/AlgorithmVisualizer/blob/master/LICENSE.txt) for more details
+
+## Issues
+
+If there is an issue with the application, please take a look at [issues](https://github.com/dlarocque/AlgorithmVisualizer/issues).  If your issue is not already listed, please create a new issue that describes your problem!
+
+## Acknowledgments
+
+Inspiration for the project https://www.youtube.com/watch?v=kPRA0W1kECg
+
+
---
+++ src/com/example/algorithmvisualizer/ArrDisplay.java
@@ -0,0 +1,123 @@
+/*
+ * An application that visualizes sorting algorithms through bar graphs.
+ * 
+ * Author : Daniel La Rocque
+ * 
+ * Date started : April 28th, 2020
+ * 
+ * This project is licensed under the MIT License.
+ * See LICENSE.txt for more details
+ * 
+ * Instructions on how to use the application are included in README.md
+ */
+package com.example.algorithmvisualizer;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ArrDisplay extends JComponent {
+
+    private static final long serialVersionUID = 1L;
+    private int swappedIndex1;
+    private int swappedIndex2;
+    private int numChunks; // amount of frames painted since the array was shuffled
+    private boolean isComplete; // if the array is sorted
+    private ArrayList<Integer[]> swappedIndexes;
+    private Integer[] arr;
+    private AlgVisualizer algVisualizer;
+    private ContentWindow frame;
+
+    public ArrDisplay(AlgVisualizer algVisualizer, ContentWindow frame) {
+        this.algVisualizer = algVisualizer;
+        this.frame = frame;
+        swappedIndexes = new ArrayList<Integer[]>();
+    }
+
+    /*
+	 * We override the paintComponent method to allow us to render our own
+	 * visualization of the array in bar graph form. This method is called through
+	 * the use of the repaint() method that is called in the SwingWorkers publish()
+	 * method. We use loops to iterate through the array and draw every index,
+	 * filling the arrPanel.
+	 * 
+	 * Since this method can take some time to execute, it may only be able to draw
+	 * a fraction of the total frames at lower delay times.
+	 * 
+	 * There is a list of swappedIndexes that contains each pair of indexes that
+	 * were swapped during sorting. Since we always draw the most recent chunk,
+	 * numChunks will always represent the number of the array clone we're drawing,
+	 * so using that to get the correct index in swappedIndexes will work.
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D graphics2d = (Graphics2D) g;
+        graphics2d.setColor(Color.DARK_GRAY);
+        graphics2d.fillRect(0, 0, frame.getArrDisplayWidth(), frame.getArrDisplayHeight());
+
+        if (algVisualizer.getSort().equals("Not Sorting") || isComplete) {
+            swappedIndex1 = -1;
+            swappedIndex2 = -1;
+        } else if (!algVisualizer.stopSort()) {
+            swappedIndex1 = swappedIndexes.get(numChunks)[0];
+            swappedIndex2 = swappedIndexes.get(numChunks)[1];
+        }
+        // Iterate through the array and draw every index
+        for (int i = 0; i < arr.length; i++) {
+            int width = (int) (frame.getArrDisplayWidth() / (double) arr.length);
+            int height = arr[i] * ((int) (frame.getArrDisplayHeight() / arr.length));
+            int x = i * width;
+            int y = frame.getArrDisplayHeight() - height;
+
+            if (i == swappedIndex1 && !algVisualizer.stopSort()) {
+                graphics2d.setColor(Color.RED);
+            } else if (i == swappedIndex2 && !algVisualizer.stopSort()) {
+                graphics2d.setColor(Color.BLUE);
+            } else if (isComplete) {
+                graphics2d.setColor(Color.GREEN);
+            } else {
+                graphics2d.setColor(Color.WHITE);
+            }
+            // Draw the current indexes bar
+            graphics2d.fillRect(x, y, width, height);
+        }
+        algVisualizer.updatePerformance();
+    }
+
+    public ArrayList<Integer[]> getSwappedIndexes() {
+        return swappedIndexes;
+    }
+
+    // Add a pair of swapped indexes to the end of the list
+    public void addSwappedIndexes(int swappedIndex1, int swappedIndex2) {
+        swappedIndexes.add(new Integer[]{swappedIndex1, swappedIndex2});
+    }
+
+    public void clearSwappedIndexes() {
+        swappedIndexes = new ArrayList<Integer[]>();
+    }
+
+    public void setNumChunk(int numChunks) {
+        this.numChunks = numChunks;
+    }
+
+    public int getNumChunks() {
+        return numChunks;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.isComplete = complete;
+    }
+
+    public Integer[] getArr() {
+        return arr;
+    }
+
+    public void setArr(Integer[] arr) {
+        this.arr = arr;
+    }
+}
