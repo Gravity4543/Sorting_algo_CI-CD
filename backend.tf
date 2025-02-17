04560f0873a0dcda37863c993fd32a56dffc288d
Shikhar vats <86479021+Gravity4543@users.noreply.github.com>
2025-02-17 18:11:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cicdbackend3"  # Replace with your S3 bucket name
-    key    = "s3://cicdbackend3/cicd_terraform/" # Path within the bucket
+    key    = "cicd_terraform/terraform.tfstate" # Path within the bucket
     region = "us-east-1" # Your AWS region
     # For state locking (recommended)
     # encrypt = true # For encrypting state at rest (recommended)
